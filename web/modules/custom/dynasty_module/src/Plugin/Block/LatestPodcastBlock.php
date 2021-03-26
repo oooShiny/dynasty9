0e0ae26c5bcd64c4b8267a845e419b04c4d12ffa
Andrew Brown <arbrown83@gmail.com>
2021-03-26 18:05:49 -0400

    Can't render full podcast node on homepage, displaying fields instead

--- web/modules/custom/dynasty_module/src/Plugin/Block/LatestPodcastBlock.php
+++ web/modules/custom/dynasty_module/src/Plugin/Block/LatestPodcastBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\dynasty_module\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a Block that displays the most recent podcast episode.
@@ -24,8 +25,23 @@ class LatestPodcastBlock extends BlockBase {
       ->sort('created' , 'DESC')
       ->range(0,1)
       ->execute();
-    $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
-    return \Drupal::entityTypeManager()->getViewBuilder('node')->view($node, 'teaser');
+    $node = Node::load(end($nid));
+
+    $link_array = parse_url($node->get('field_mp3')->value);
+    $path_array = explode('/', $link_array['path']);
+    $mp3 = $path_array[6];
+    $episode = [
+      'title' => $node->label(),
+      'id' => $node->id(),
+      'mp3' => $mp3,
+      'duration' => $node->get('field_duration')->value,
+      'description' => $node->get('body')->value
+    ];
+
+    return [
+      '#theme' => 'latest_podcast_block',
+      '#episode' => $episode
+    ];
   }
 
 }
