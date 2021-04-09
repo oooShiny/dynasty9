616806a8e23fdc5d20b97e82a285d43e782953d5
Andrew Brown <arbrown83@gmail.com>
2021-04-09 13:58:54 -0400

    Podcast search changes

--- web/themes/custom/dynasty/js/playpodcast.js
+++ web/themes/custom/dynasty/js/playpodcast.js
@@ -2,17 +2,11 @@ let playing = true;
 let pPause = document.querySelector('#play-pause');
 function playPause(id) {
   if (playing) {
-    const song = document.querySelector('#'+id),
-      thumbnail = document.querySelector('#thumbnail');
-    pPause.src = "./assets/icons/pause.png"; //this will change the Play button to a Pause button
-    thumbnail.style.transform = "scale(1.15)"; //this will slightly zoom in the album cover for a cool effect
-    song.play(); //this will play the audio track
+    const pod_ep = document.querySelector('#'+id);
+    pod_ep.play(); //this will play the audio track
     playing = false;
   } else {
-    pPause.src = "./assets/icons/play.png"
-    thumbnail.style.transform = "scale(1)" //this will slightly zoom in the album cover for a cool effect
-
-    song.pause();
+    pod_ep.pause();
     playing = true;
   }
 }
