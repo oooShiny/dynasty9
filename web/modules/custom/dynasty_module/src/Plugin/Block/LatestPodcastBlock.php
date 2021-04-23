d95a003822a17633c0a451aa1db765695244bf6f
Andrew Brown <arbrown83@gmail.com>
2021-04-23 12:27:52 -0400

    Updating front page podcast player to custom one in the hopes that the home page will load more quickly

--- web/modules/custom/dynasty_module/src/Plugin/Block/LatestPodcastBlock.php
+++ web/modules/custom/dynasty_module/src/Plugin/Block/LatestPodcastBlock.php
@@ -30,12 +30,16 @@ class LatestPodcastBlock extends BlockBase {
     $link_array = parse_url($node->get('field_mp3')->value);
     $path_array = explode('/', $link_array['path']);
     $mp3 = $path_array[6];
+
     $episode = [
       'title' => $node->label(),
+      'subtitle' => $node->get('field_subtitle')->value,
       'id' => $node->id(),
       'mp3' => $mp3,
+      'mp3_url' => $node->get('field_mp3')->value,
       'duration' => $node->get('field_duration')->value,
-      'description' => $node->get('body')->value
+      'description' => $node->get('body')->value,
+      'ep_art' => $node->get('field_episode_cover_image')->value
     ];
 
     return [
