0a4e4133fd83c0befdb1fb2dc7427ec60ef3082f
Andrew Brown <arbrown83@gmail.com>
2022-01-13 15:32:14 -0500

    Redoing On This Day section of homepage.

--- web/modules/custom/dynasty_module/src/Plugin/Block/OnThisDayBlock.php
+++ web/modules/custom/dynasty_module/src/Plugin/Block/OnThisDayBlock.php
@@ -28,7 +28,7 @@ class OnThisDayBlock extends BlockBase {
 
     $games = \Drupal::entityTypeManager()
       ->getStorage('node')
-      ->loadMultiple($game_nids);
+      ->loadMultiple(array_keys($game_nids));
 
     $build = [
       '#theme' => 'on_this_day_block',
@@ -37,10 +37,33 @@ class OnThisDayBlock extends BlockBase {
     foreach ($games as $game) {
       $game_date = substr($game->get('field_date')->value, 5);
       if ($game_date == $today) {
-        $render_controller = \Drupal::entityTypeManager()->getViewBuilder($game->getEntityTypeId());
-        $build['#games'][] = $render_controller->view($game, 'card');
+        // Find highlights for this game.
+        $highlight_nid = \Drupal::entityQuery('node')
+          ->condition('type', 'highlight')
+          ->condition('field_play_of_the_game', 1)
+          ->condition('field_game', $game->id())
+          ->execute();
+
+        $highlight = \Drupal::entityTypeManager()
+          ->getStorage('node')
+          ->load(end($highlight_nid));
+
+        $opponent = explode(' ', $game->field_opponent->entity->label());
+        $css = strtolower(implode('-', $opponent));
+        $muse_id = $highlight->field_muse_video_id->value ?? '';
+        $build['#games'][] = [
+          'alias' => \Drupal::service('path_alias.manager')->getAliasByPath('/node/'.$game->id()),
+          'pats_score' => $game->field_patriots_score->value,
+          'opp_score' => $game->field_opponent_score->value,
+          'opponent' => end($opponent),
+          'season' => $game->field_season->value,
+          'week' => $game->field_week->entity->label(),
+          'css' => $css,
+          'highlight' => $muse_id,
+        ];
       }
     }
+
     return $build;
   }
 
